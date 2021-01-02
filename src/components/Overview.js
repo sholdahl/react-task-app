defa6b88f08a4f6f76140ff30dd318ef96bfad48
sholdahl <sam.holdahl@gmail.com>
2021-01-02 12:48:16 -0600

    first working version

---
+++ src/components/Overview.js
@@ -0,0 +1,18 @@
+// Overview.js
+
+import React from "react";
+import uniqid from "uniqid";
+
+const Overview = (props) => {
+  const { tasks } = props;
+
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return <li key={uniqid()}>{task}</li>;
+      })}
+    </ul>
+  );
+};
+
+export default Overview;
